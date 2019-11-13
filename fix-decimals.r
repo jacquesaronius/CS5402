523afb5e304043ff49b44c4c5093ddfa14a5357b
Jacques Bourquin <jacquesaronius@gmail.com>
2019-11-13 12:57:18 -0600

    Changed heuristic to calculate tolerance based on the sum of the two values compared to column average

--- fix-decimals.r
+++ fix-decimals.r
@@ -5,11 +5,13 @@ df = data.frame(d)
 cat("Read in CSV file\n")
 for(i in 1:ncol(df))
 {
-    for (j in 1:nrow(df))
-    {
-        if (sapply(df[i], class) != "numeric") {
+    if (sapply(df[i], class) != "numeric") {
             next();
-        }
+    }
+    avg = mean(df[,i], na.rm=T)
+    #print(paste0(avg))
+    for (j in 3:nrow(df))
+    {
         if (j > 1 & j < nrow(df))
         {
             if (!is.na(df[j, i]))
@@ -18,16 +20,26 @@ for(i in 1:ncol(df))
                 #Find a previous value that is not NA
                 while (is.na(df[j - k, i])) {
                     k = k + 1
+                    #print(paste0(i, " ", j, " ", k, " ", j - k))
                 }
                 
+                # If there is a value difference greater than our tolerance 
+                # but it is legitimate we could get caught in an infinite loop
+                value = df[j, i]
                 iterations = 0
-                while (abs(df[j, i] - df[j - k, i]) > 5) {
+                while (abs(df[j, i] + df[j - k, i]) < avg * 0.1 & abs(df[j, i] + df[j - k, i]) != df[j, i] * 2 & iterations < 1000) {
                     if (df[j, i] < df[j - k, i]) {
                         df[j, i] = df[j, i] * 10
                     }
                     else {
                        df[j, i] = df[j, i] / 10 
                     }
+                    iterations = iterations + 1
+                }
+
+                if (iterations == 1000) {
+                    print(paste0(df[j, i], " ", df[j - k, i],"Exceeded iteration limit - reverting"))
+                    df[j, i] = value
                 }
             }
         }
